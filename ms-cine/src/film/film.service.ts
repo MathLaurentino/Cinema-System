4afeb5a756acc38e340022884290b4ac63583afc
Matheus Laurentino Alves Barbosa <matheus.laurentino@compasso.com.br>
2024-11-19 17:33:09 -0300

    film update endpoint

--- ms-cine/src/film/film.service.ts
+++ ms-cine/src/film/film.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateFilmDto } from './dto/create-film.dto';
 import { UpdateFilmDto } from './dto/update-film.dto';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -15,42 +15,59 @@ export class FilmService {
   @InjectRepository(Genre)
   private readonly genreRepository: Repository<Genre>;
 
-  async create(createFilmDto: CreateFilmDto) {
+  async create(createFilmDto: CreateFilmDto): Promise<Film> {
     const genres = await Promise.all(
       createFilmDto.genres.map((name) => this.preloadGenreByName(name)),
     );
-
+    
     const film = await this.filmRepository.create({
       ...createFilmDto,
       genres
     })
-
+    
     return this.filmRepository.save(film);
   }
+
 
   async findAll(): Promise<Film[]> {
     return await this.filmRepository.find({ relations: ['genres'] })
   }
 
+
   findOne(id: number) {
     return `This action returns a #${id} film`;
   }
 
-  update(id: number, updateFilmDto: UpdateFilmDto) {
-    return `This action updates a #${id} film`;
+
+  async update(id: number, updateFilmDto: UpdateFilmDto): Promise<Film> {
+    let  genres 
+    if (updateFilmDto.genres != null) 
+      genres = await Promise.all(updateFilmDto.genres.map((name) => this.preloadGenreByName(name)))
+    
+    const film = await this.filmRepository.preload({
+      ... updateFilmDto,
+      genres, 
+      id
+    })
+    
+    if (!film) 
+      throw new NotFoundException(`Film ID ${id} not found`);
+    
+    return this.filmRepository.save(film);
   }
+
 
   remove(id: number) {
     return `This action removes a #${id} film`;
   }
-
+  
   private async preloadGenreByName(name: string): Promise<Genre> {
     const genre = await this.genreRepository.findOne({ where: { name } });
-
+    
     if (genre) {
       return genre;
     }
-
+    
     return this.genreRepository.create({ name });
   }
 }
